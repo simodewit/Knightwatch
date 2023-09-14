5236b563e35824f2be3f603a042d8b942694c585
RyanR06 <RyanRoelofs19032006@gmail.com>
2023-09-14 14:54:10 +0200

    NPCScript
    Npc kan nu naar kasteel. Update noodzakelijk voor verbetering.

---
+++ KnightFall/Assets/Scripts/NPCScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Jobs;
+
+public class NPCScript : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public Transform wall;
+    public Transform castle;
+    public NavMeshAgent agent;
+    public int areaMask;
+    public NavMeshPath path;
+
+
+    void Start()
+    {
+        path = new NavMeshPath();
+    }
+
+    private void chase()
+    { 
+
+        
+    }
+
+    void Update()
+    {
+        NavMesh.CalculatePath(startPosition, castle.position, areaMask, path);
+        agent.SetPath(path);
+        NavMeshBuilder.BuildNavMesh()
+    }
+
+
+
+
+
+
+}
