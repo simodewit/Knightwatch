9f98e5676bf8ca11368f44b726cfe7b4639a2c42
simodewit <s.f.buikema@gmail.com>
2023-10-02 15:41:07 +0100

    map begin

--- KnightFall/Assets/Scripts/CameraFollow.cs
+++ KnightFall/Assets/Scripts/CameraFollow.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -10,7 +12,26 @@ public class CameraFollow : MonoBehaviour
     public Rigidbody rb;
     public Vector3 offset;
     public float lerpspeed;
+    public GameObject empty;
+    public InputMaster input;
+    public InputAction move;
 
+    private void Awake()
+    {
+        input = new InputMaster();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        move = input.Movement3rdperson.Movement;
+        move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+    }
 
     public void Start()
     {
@@ -20,7 +41,7 @@ public class CameraFollow : MonoBehaviour
 
     void Update()
     {   
-        Vector3 v = new Vector3(player.transform.position.x +offset.x, player.transform.position.y +offset.y, player.transform.position.z +offset.z);
-        transform.position = Vector3.Lerp(transform.position, v, lerpspeed * Time.deltaTime);
+        Vector3 playerPositionWithOffset = new Vector3(player.transform.position.x +offset.x, player.transform.position.y +offset.y, player.transform.position.z +offset.z);
+        transform.position = Vector3.Lerp(transform.position, playerPositionWithOffset, lerpspeed * Time.deltaTime);
     }
 }
